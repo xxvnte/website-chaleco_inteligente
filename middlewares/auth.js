57b78eb880d38e4d4620af33c77f65fdcb234f23
vicente <108952417+xxvnte@users.noreply.github.com>
2024-07-19 14:45:17 -0400

    Add files via upload

---
+++ middlewares/auth.js
@@ -0,0 +1,41 @@
+const jwt = require('jsonwebtoken');
+
+const authenticate = (req, res, next) => {
+  const token = req.cookies.token;
+  if (!token) {
+    req.session.destroy((err) => {
+      if (err) {
+        console.log('error destroying session:', err);
+      }
+      res.clearCookie('token');
+      return res.redirect('/login');
+    });
+  } else {
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+      if (err) {
+        req.session.destroy((err) => {
+          if (err) {
+            console.error('error destroying session:', err);
+          }
+          res.clearCookie('token');
+          return res.redirect('/login');
+        });
+      } else {
+        if (!req.session.isAuthenticated || req.session.userId !== decoded.id) {
+          req.session.destroy((err) => {
+            if (err) {
+              console.error('error destroying session:', err);
+            }
+            res.clearCookie('token');
+            return res.redirect('/login');
+          });
+        } else {
+          req.user = decoded;
+          next();
+        }
+      }
+    });
+  }
+};
+
+module.exports = authenticate;
