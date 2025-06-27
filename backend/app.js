4610531727ad0d2430ef7b4e958e1cf41b1b2a6e
xxvnte <vicente.leiva2@mail.udp.cl>
2025-06-27 15:57:46 -0400

    minor changes

--- backend/app.js
+++ backend/app.js
@@ -22,7 +22,7 @@ app.use(
     secret: process.env.JWT_SECRET,
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: false, sameSite: "lax", httpOnly: true },
+    cookie: { secure: false, sameSite: "none" },
   })
 );
 
